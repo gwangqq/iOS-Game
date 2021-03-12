69de45e5a4316430258493b3039e9a5277b4ca7b
gwanggyu park <pgg961@gmail.com>
2021-03-12 22:38:45 +0900

    comparing two cards

--- ContentView.swift
+++ ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
         ZStack{
             Image("background")
@@ -19,13 +25,33 @@ struct ContentView: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
-                Image("dealbutton")
+                Button(action: {
+                    // Create a ramdom number betwenn 2 and 14
+                    let playerRand = Int.random(in: 2 ... 14)
+                    let cpuRand = Int.random(in: 2 ... 14)
+                    
+                    // Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    // Update the score
+
+                    if playerRand > cpuRand{
+                        playerScore += 1
+                    }
+                    else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+                
                 Spacer()
                 HStack{
                     Spacer()
@@ -33,7 +59,7 @@ struct ContentView: View {
                         Text("Player")
                             .font(.headline)
                             .foregroundColor(Color.white)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
@@ -42,7 +68,7 @@ struct ContentView: View {
                         Text("CPU")
                             .font(.headline)
                             .foregroundColor(Color.white)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
